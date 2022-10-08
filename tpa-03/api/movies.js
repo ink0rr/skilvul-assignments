e0607c8dd02b01c19a54bd78069b026ea4455ab8
ink0rr <tomflynnbeltsazar@gmail.com>
2022-10-08 15:37:14 +0700

    update tpa-03

---
+++ tpa-03/api/movies.js
@@ -0,0 +1,10 @@
+const API_KEY = "7ea6be3209c62292770bb549f6a0eac1"
+
+export async function getMovies(search) {
+  const res = await fetch(
+    search
+      ? `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${search}`
+      : `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+  )
+  return await res.json()
+}
---
+++ tpa-03/script.js
@@ -0,0 +1,29 @@
+import { getMovies } from "./api/movies.js"
+import { getParams } from "./utils/getParams.js"
+
+const params = getParams(window.location.search)
+
+document.getElementById("search").addEventListener("submit", (event) => {
+  event.preventDefault()
+  const data = new FormData(event.target)
+  const query = data.get("query")
+  location.search = `?query=${query}`
+})
+
+// Load movies
+;(async function () {
+  const movieList = document.querySelector(".movie-list")
+  const movies = await getMovies(params.query)
+  movies.results.forEach((movie) => {
+    const date = new Date(movie.release_date)
+
+    movieList.innerHTML += /*html*/ `
+      <a class="movie-container" href="movies/?id=${movie.id}">
+        <img class="movie-thumbnail" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" />
+        <div class="movie-content">
+          <p>${movie.title}</p>
+          <p>${date.toDateString()}</p>
+        </div>
+      </a>`
+  })
+})()
